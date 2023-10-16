10a3345b2bdfa8f9e5e79d816d84f41e26d3e972
DangTrungHieu1 <104317311+DangTrungHieu1@users.noreply.github.com>
2023-10-16 13:04:29 +0700

    Add project files.

---
+++ WebApplicationProject/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplicationProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        public ActionResult Course()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult Teacher()
+        {
+            return View();
+        }
+
+        public ActionResult Price()
+        {
+            return View();
+        }
+
+        public ActionResult Review()
+        {
+            return View();
+        }
+    }
+}
