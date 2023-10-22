d36d5e9fa60e58d48a33f1cd3ec0b5edf68b0c36
2124801030177 <2124801030177@student.tdmu.edu.vn>
2023-10-22 14:40:36 +0700

    S1
    Update

---
+++ WebApplicationProject/Areas/Admin/Controllers/RegisterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplicationProject.Areas.Admin.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Admin/Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
