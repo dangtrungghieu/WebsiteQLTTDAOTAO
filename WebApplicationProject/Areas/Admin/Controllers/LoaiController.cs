89d2617200aa989cec255e72ec166e1c9a813a46
dangtrungghieu <dang.trungghieu@gmail.com>
2023-11-04 10:01:29 +0700

    Update - 04112023

--- WebApplicationProject/Areas/Admin/Controllers/LoaiController.cs
+++ WebApplicationProject/Areas/Admin/Controllers/LoaiController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using WebApplicationProject.Model;
 namespace WebApplicationProject.Areas.Admin.Controllers
 {
     public class LoaiController : Controller
     {
+        //Khai bao CSDL
+        FINALPROJECTEntities db = new FINALPROJECTEntities();
         // GET: Admin/Loai
         public ActionResult Index()
         {
-            return View();
+            var loai = db.LOAI.Select(n => n);
+            return View(loai.OrderBy(n => n.MaLoai));
         }
     }
 }
