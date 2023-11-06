0bec2d3ee1a7bec5d5ec9f4f88005edd5199b539
2124801030177 <2124801030177@student.tdmu.edu.vn>
2023-11-06 15:00:47 +0700

    Version1

--- WebApplicationProject/Areas/User/Controllers/UserController.cs
+++ WebApplicationProject/Areas/User/Controllers/UserController.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplicationProject.Model;
 
 namespace WebApplicationProject.Areas.User.Controllers
 {
     public class UserController : Controller
     {
+        FINALPROJECTEntities db = new FINALPROJECTEntities();
         // GET: User/User
-        public ActionResult Index()
+        public ActionResult Profile()
         {
             return View();
         }
--- WebApplicationProject/Model/FinalProject.Designer.cs
+++ WebApplicationProject/Model/FinalProject.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Hieu\OneDrive\Máy tính\ProJectLTW\WebApplicationProject\WebApplicationProject\Model\FinalProject.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\ADMIN\Documents\Gits\WebsiteQLTTDAOTAO\WebApplicationProject\Model\FinalProject.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
