aad81e13a4604bd25ddc7ab8c91428a17adcb0c8
PaperGun <zipberhaihuoc@gmail.com>
2023-11-13 15:13:28 +0700

    up

---
+++ WebApplicationProject/Areas/User/Controllers/CourseUserController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationProject.Model;
+
+namespace WebApplicationProject.Areas.User.Controllers
+{
+    public class CourseUserController : Controller
+    {
+        FINALPROJECTEntities db = new FINALPROJECTEntities();
+        // GET: User/CourseUser
+        public ActionResult MyCourse()
+        {
+            var hocvien = from hv in db.DANGKY.Where(n => n.MaHocVien_DangKy == 4) select hv;
+            return View(hocvien);
+        }
+
+        public ActionResult MyWishList()
+        {
+            var khoahoc = from hv in db.DANGKY.Where(n => n.MaHocVien_DangKy == 4) select hv;
+            return View(khoahoc);
+        }
+
+        public ActionResult DetailsCourse(int? id)
+        {
+            var khoahoc = db.KHOAHOC.Find(id);
+            return View(khoahoc);
+        }
+    }
+}
--- WebApplicationProject/Areas/User/Controllers/UserController.cs
+++ WebApplicationProject/Areas/User/Controllers/UserController.cs
@@ -11,11 +11,35 @@ namespace WebApplicationProject.Areas.User.Controllers
     {
         FINALPROJECTEntities db = new FINALPROJECTEntities();
         // GET: User/User
-        public ActionResult Profile()
+        [HttpGet]
+        public ActionResult usProfile()
         {
             var hocvien = db.HOCVIEN.FirstOrDefault(hv => hv.MaHocVien == 4);
-            return View(hocvien); 
+            return View(hocvien);
         }
+
+        // Action HTTP POST để xử lý cập nhật thông tin profile
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult usProfile(FormCollection f)
+        {
+            var hocvien = db.HOCVIEN.FirstOrDefault(hv => hv.MaHocVien == 4);
+
+            if (ModelState.IsValid)
+            {
+                hocvien.TenHocVien = f["sTenHocVien"];
+                hocvien.DiaChi = f["sDiaChi"];
+                hocvien.Email = f["sEmail"];
+                hocvien.SoDienThoai = int.Parse(f["sSoDienThoai"]);
+
+                db.SaveChanges();
+
+                return RedirectToAction("Profile");
+            }
+
+            return View(hocvien);
+        }
+
         // GET: User/Login
         public ActionResult Login()
         {
@@ -26,5 +50,53 @@ namespace WebApplicationProject.Areas.User.Controllers
         {
             return View();
         }
+        [ChildActionOnly]
+        public ActionResult NavUser()
+        {
+            var hocvien = db.HOCVIEN.FirstOrDefault(hv => hv.MaHocVien == 4);
+            return PartialView(hocvien);
+        }
+        public ActionResult NavHeaderUser()
+        {
+            return View();
+        }
+        public ActionResult Schedule()
+        {
+            //Hiển thị Calendar
+            CalendarViewModel model = new CalendarViewModel();
+            // Tính toán StartDate và DisplayDates
+            DateTime today = DateTime.Now;
+            DateTime startDate = today.AddDays(-(int)today.DayOfWeek);
+            DateTime endDate = startDate.AddDays(20);
+
+            model.StartDate = startDate;
+            model.DisplayDates = Enumerable.Range(0, (int)(endDate - startDate).TotalDays + 1)
+                .Select(offset => startDate.AddDays(offset))
+                .ToList();
+
+            // Thêm thông tin về giờ học
+            model.ClassesByDate = new Dictionary<DateTime, List<ClassInfo>>();
+            foreach (var date in model.DisplayDates)
+            {
+                List<ClassInfo> classesForDate = GetClassesForDate(date);
+                model.ClassesByDate[date] = classesForDate;
+            }
+
+            return View(model);
+        }
+        private List<ClassInfo> GetClassesForDate(DateTime date)
+        {
+
+            return db.DANGKY
+                .Where(c => c.LOP.NgayKhaiGiang <= date && c.LOP.NgayKetThuc >= date && (c.LOP.CAHOC.NgayHocTrongTuan.Contains(((int)date.DayOfWeek + 1).ToString())) && c.MaHocVien_DangKy == 6)
+                .Select(c => new ClassInfo
+                {
+                    ClassName = c.LOP.TenLop,
+                    ClassTenCaHoc = c.LOP.CAHOC.TenCaHoc,
+                    ClassGiaoVien = c.LOP.NHANVIEN.TenNhanVien
+                })
+                .ToList();
+        }
+
     }
 }
